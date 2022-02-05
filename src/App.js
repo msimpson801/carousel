14d3602c8d8ed5aa298399cb8c968aca4d4a427f
Mark Simpson <msimpson801@gmail.com>
2022-02-05 20:15:55 +0000

    fixing slider button to only work for number of people

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import {MySlider} from "./MySlider";
 
+//9 = 5
+// 8 = 4
+//7 = 3
+// 6 = 2
+// 5 = 1
+// 4 = 0
+
+
 const info = [
     {name: "Mark"},
     {name: "Darrel"},
     {name: "Dave"},
     {name: "Brendan"},
-    {name: "Paddy"}
+    {name: "Neill"},
+    {name: "Kavyaa"},
+    {name: "Judith"},
 ]
 function App() {
   return (
--- src/MySlider.js
+++ src/MySlider.js
@@ -3,6 +3,7 @@ import {useState} from "react";
 
 
 function shift(props) {
+        console.log(props.clickCount , "Here is my click count")
         if (props.clickCount !== 0) {
             return `-${props.clickCount * 25}%`
         }
@@ -106,7 +107,7 @@ function MySlider(props) {
                         })
                     }
                 </ImageContainer>
-                {clickCount < props?.info?.length / 2 && <RightButton onClick={()=> {handleRightClick()}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#6563ff" d="M14.12109,17.24268a.99678.99678,0,0,1-.707-.293L9.17188,12.707a.99962.99962,0,0,1,0-1.41406l4.24218-4.24268a.99989.99989,0,0,1,1.41406,1.41407L11.293,12l3.53515,3.53564a1,1,0,0,1-.707,1.707Z"/></svg></RightButton>}
+                {clickCount < Math.floor(props?.info?.length - 4) && <RightButton onClick={()=> {handleRightClick()}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#6563ff" d="M14.12109,17.24268a.99678.99678,0,0,1-.707-.293L9.17188,12.707a.99962.99962,0,0,1,0-1.41406l4.24218-4.24268a.99989.99989,0,0,1,1.41406,1.41407L11.293,12l3.53515,3.53564a1,1,0,0,1-.707,1.707Z"/></svg></RightButton>}
             </MainContainer>
         </>
 
